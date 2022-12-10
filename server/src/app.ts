bef6be9d43d24e5dbf0f9038e0f9f25a69407127
maidaamahmud <maidaamahmud@gmail.com>
2022-12-10 20:21:02 +0000

    minor changes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,8 @@
 import express, { Express } from "express";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 import cors from "cors";
 import projectRoutes from "./routes";
 
-const mongoose = require("mongoose");
 const app: Express = express();
 
 const PORT: string | number = process.env.PORT || 4000;
@@ -22,7 +21,8 @@ mongoose
   })
   .catch((err: Error) => {
     console.log(`ERROR IS: ${err.message}`);
-    process.exit(-1);
+    //process.exit(-1);
+    throw err;
   });
 
 module.exports = mongoose;
